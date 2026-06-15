10a841d23312776f8dca42affc0d53fc80720aa0
softnotsoft <guipereiramartins.gm@gmail.com>
2026-06-15 22:52:45 +0100

    Add HousingTheme and UI background assets
    Introduce a HousingTheme ScriptableObject (HousingTheme.cs) with color properties and add a serialized HousingTheme.asset instance with preset colors. Add UI-related folders and .meta files, and update UI_Main.unity to include a new Background GameObject (RectTransform, Image, CanvasRenderer) so the project has a centralized UI theme and a scene background element.

---
+++ Assets/HousingAI/Scripts/UI/HousingTheme.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HousingTheme", menuName = "HousingAI/UI Theme")]
+public class HousingTheme : ScriptableObject
+{
+    public Color Primary;
+    public Color PrimaryDarker;
+    public Color Secondary;
+    public Color Background1;
+    public Color Background2;
+    public Color Gray;
+    public Color Accent;
+    public Color AccentHover;
+}
